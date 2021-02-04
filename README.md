71c2f33ebe237313730e46c7c899626b9fe5b0ba
Flagon00 <gift256@outlook.com>
2021-02-04 20:52:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<p align="center">
+  <img src="https://i.imgur.com/IFEp5Cq.png" alt="Logo" width="80" height="80">
+  <h3 align="center">Golang Tools</h3>
+  <p align="center">
+    Just import and use!
+    </br>
+    </br>
+    <a href="https://pkg.go.dev/github.com/Flagon00/Tools"><img src="https://godoc.org/github.com/xta/okrun?status.svg"></a>
+  </p>
+</p>
---
+++ tools.go
@@ -0,0 +1,163 @@
+package tools
+
+import (
+	"math/rand"
+	"strings"
+	"errors"
+	"bufio"
+	"time"
+	"fmt"
+	"os"
+)
+
+// Flush append data to file
+func Flush(filePath string, str string) error {
+	fileHandle, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		// If file doesn't exist he create him
+		if os.IsNotExist(err) {
+			fileHandle, err = os.Create(filePath)
+			if err != nil{
+				return err
+			}
+		} else{
+			return err
+		}
+
+	}
+
+	// Closing the file at the end of the function
+	defer fileHandle.Close()
+
+	// Flush data to file
+	fileHandle.WriteString(str)
+
+	return nil
+}
+
+// FileContains check that string exist in file
+func FileContains(filePath string, str string) (bool, error) {
+	fileHandle, err := os.Open(filePath)
+	if err != nil {
+		return false, err
+	}
+
+	// Closing the file at the end of the function
+	defer fileHandle.Close()
+
+	// Making loop for file lines
+	fileHandleScan := bufio.NewScanner(fileHandle)
+	for fileHandleScan.Scan() {
+		if strings.Contains(fileHandleScan.Text(), str) {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+// FileLinesCounter counts lines of file
+func FileLinesCounter(filePath string) (int, error) {
+	fileHandle, err := os.Open(filePath)
+	if err != nil {
+		return 0, err
+	}
+
+	// Closing the file at the end of the function
+	defer fileHandle.Close()
+
+	// Preparing variable for counting lines
+	var lines int
+
+	// Making loop for file lines
+	fileHandleScan := bufio.NewScanner(fileHandle)
+	for fileHandleScan.Scan() {
+		lines++
+	}
+
+	return lines, err
+}
+
+// SpecifyLineByNumber return specific text of line in file
+func SpecifyLineByNumber(filePath string, specifyLine int) (string, error) {
+	fileHandle, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	// Closing the file at the end of the function
+	defer fileHandle.Close()
+
+	// Preparing variable for result
+	var lines int
+
+	// Making loop for file lines
+	fileHandleScan := bufio.NewScanner(fileHandle)
+	for fileHandleScan.Scan() {
+		if lines <= specifyLine{
+			// Return result with removed all whitespaces and suffixes
+			return fmt.Sprint(strings.TrimSpace(fileHandleScan.Text())), nil
+		}
+
+		lines++
+	}
+
+	return "", errors.New("The line cannot be found")
+}
+
+// SpecifyLineByText return specific text of line in file
+func SpecifyLineByText(filePath string, str string) (string, error) {
+	fileHandle, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	// Closing the file at the end of the function
+	defer fileHandle.Close()
+
+	// Making loop for file lines
+	fileHandleScan := bufio.NewScanner(fileHandle)
+	for fileHandleScan.Scan() {
+		// Return result with removed all whitespaces and suffixes
+		if strings.Contains(fileHandleScan.Text(), str) {
+			return fmt.Sprint(strings.TrimSpace(fileHandleScan.Text())), nil
+		}
+	}
+
+	return "", errors.New("The line cannot be found")
+}
+
+// Exists check a file exists
+func Exists(filePath string) (bool, error) {
+	 _, err := os.Stat(filePath)
+	 if err != nil{
+	 	// Not exist
+	 	if os.IsNotExist(err) {
+			return false, nil
+		}
+
+		return false, err
+	 }
+
+	// Exist
+	return true, nil
+}
+
+// GetStringInBetween return string between two characters
+func GetStringInBetween(str string, homeChar string, endChar string) (string, error) {
+	home := strings.Index(str, homeChar)
+	if home == -1 {
+		return "", errors.New("No strings to find")
+	}
+	home += len(homeChar)
+
+	end := strings.Index(str, endChar)
+
+	return str[home:end], nil
+}
+
+// Random return random numbers in rage
+func Random(min int, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min) + min
+}
